73a61332574863194d531b30587883cd4ec31db8
asv02 <200106025@hbtu.ac.in>
2025-05-31 20:51:39 +0530

    parcel

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-heading = React.createElement('h1',{id:"heading1"},"Hello world from react")
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+var heading = React.createElement('h1',{id:"heading1"},"Hello world from react")
 
 //Nested elements
-h1 = React.createElement('h1',{id:"h1"},"This is heading under three divs")
-div3 = React.createElement('div',{id:"h1"},h1)
-div2 = React.createElement('div',{id:"div2"},div3)
-div1 = React.createElement('div',{id:"div1"},div2)
+var h1 = React.createElement('h1',{id:"h1"},"This is heading under three divs")
+var div3 = React.createElement('div',{id:"h1"},h1)
+var div2 = React.createElement('div',{id:"div2"},div3)
+var div1 = React.createElement('div',{id:"div1"},div2)
 
 
-root = ReactDOM.createRoot(document.getElementById('root'))
+var root = ReactDOM.createRoot(document.getElementById('root'))
 console.log("root->",root)
 root.render(h1)
