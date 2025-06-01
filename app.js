b5b68eada7b2b918070deca266a322ce8e88f9f7
asv02 <200106025@hbtu.ac.in>
2025-06-01 10:55:35 +0530

    functional component

--- app.js
+++ app.js
@@ -1,15 +1,60 @@
-import React from "react";
-import ReactDOM from 'react-dom/client'
-
-var heading = React.createElement('h1',{id:"heading1"},"Hello world from react")
-
-//Nested elements
-var h1 = React.createElement('h1',{id:"h1"},"This is heading under three divs")
-var div3 = React.createElement('div',{id:"h1"},h1)
-var div2 = React.createElement('div',{id:"div2"},div3)
-var div1 = React.createElement('div',{id:"div1"},div2)
+import React, { Component } from "react";
+import ReactDOM from "react-dom/client";
 
 
-var root = ReactDOM.createRoot(document.getElementById('root'))
-console.log("root->",root)
-root.render(h1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Functional Component
+
+
+//React Element
+// const jsxheading = (
+//   <h1 className="head" id="heading1">
+//     Element inside component.
+//   </h1>
+// );
+
+// const Title = () => <h2>Welcome to the earth</h2>;
+
+// //React Component
+// const HeadingComponent = () => (
+//   <div id="container">
+//     <Title />
+//     {(() => {
+//       console.log("React");
+//       console.log("babel");
+//       return null;
+//     })()}
+//     {jsxheading}
+//     <h1>Welcome to the react component!!</h1>
+//   </div>
+// );
+
+// console.log("jsxheading->", jsxheading);
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<HeadingComponent />);
+
+// //CORE REACT
+// var heading = React.createElement('h1',{id:"heading1"},"Hello world from react")
+
+// //Nested elements
+// var h1 = React.createElement('h1',{id:"h1"},"This is heading under three divs")
+// var div3 = React.createElement('div',{id:"h1"},h1)
+// var div2 = React.createElement('div',{id:"div2"},div3)
+// var div1 = React.createElement('div',{id:"div1"},div2)
+
+// var root = ReactDOM.createRoot(document.getElementById('root'))
+// console.log("root->",root)
+// root.render(h1)
